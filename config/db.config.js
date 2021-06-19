517a58f4eec427abed231035e386b57443e47d08
nevalenaginda <nevalenaginda10@gmail.com>
2021-06-19 17:51:05 +0700

    try fix cannot connect database

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // url: "mongodb://localhost:27017/collect_idstack",
-    url: process.env.mongoDBUri
+    url: "mongodb+srv://nevalen-1532:aginda123@cluster0.mh3dq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 }
+// url: "mongodb://localhost:27017/collect_idstack",
