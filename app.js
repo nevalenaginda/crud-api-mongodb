c0d27f34cf6bbfe59172734b0f0eaf79237cd891
nevalenaginda <nevalenaginda10@gmail.com>
2021-06-19 18:07:19 +0700

    add default host

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ app.get("/", (req, res) => {
 require("./app/routes/post.routes")(app)
 const PORT = 8000
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
