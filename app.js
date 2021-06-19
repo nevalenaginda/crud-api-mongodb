cd901c65a2321e0485c07fc2174ae79507333553
nevalenaginda <nevalenaginda10@gmail.com>
2021-06-19 20:34:13 +0700

    try to fix bug

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ app.get("/", (req, res) => {
 })
 
 require("./app/routes/post.routes")(app)
-const PORT = 8000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('Server telah di-aktivasi')
 })
